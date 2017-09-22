3e38b99eef6bc85c38cca43ac7ddce86487b7615
root <georgedelacruz213@gmail.com>
2017-09-22 23:11:58 +0800

    Added jframe for dentist information

--- Login_MVC/src/viewdao/frm_mainpage.java
+++ Login_MVC/src/viewdao/frm_mainpage.java
@@ -7,10 +7,14 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.JTextField;
 
 public class frm_mainpage extends JFrame {
 
 	private JPanel contentPane;
+	private JTable table;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -40,8 +44,21 @@ public class frm_mainpage extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel lblHello = new JLabel("Hello");
-		lblHello.setBounds(907, 35, 70, 15);
+		lblHello.setBounds(918, 33, 70, 15);
 		contentPane.add(lblHello);
+		
+		table = new JTable();
+		table.setBounds(55, 170, 898, 284);
+		contentPane.add(table);
+		
+		textField = new JTextField();
+		textField.setBounds(162, 52, 128, 29);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblDate = new JLabel("Date");
+		lblDate.setBounds(109, 59, 70, 15);
+		contentPane.add(lblDate);
 		setLocationRelativeTo(null);
 	}
 }
